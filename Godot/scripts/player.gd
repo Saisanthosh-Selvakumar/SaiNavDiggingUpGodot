e59967080d50830d73365cb0d5db7514e65d7bbd
Saisanthosh-Selvakumar <sai.primary.12@gmail.com>
2026-02-28 17:34:02 -0600

    save issue, made physics edits

--- Godot/scripts/player.gd
+++ Godot/scripts/player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
+const SPEED = 150.0
+const JUMP_VELOCITY = -200.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -27,4 +27,4 @@ func on_vignettearea_body_entered(body: Node2D) -> void:
 		ColorRect.visible = true
 		print("Vignette visible")
 		ColorRect.modulate.a = 0.0
-		create_tween().tween_property(ColorRect, "modulate:a", 1.0, 0.4)
+		create_tween().tween_property(ColorRect, "modulate:a", 10, 0.5)
